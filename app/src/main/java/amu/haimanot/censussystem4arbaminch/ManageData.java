9037557ec723a895a303d2d098838046df361567
nahomwosenu <nahometete@yahoo.com>
2017-05-09 19:34:35 +0300

    Improved Searching Algorithm

--- app/src/main/java/amu/haimanot/censussystem4arbaminch/ManageData.java
+++ app/src/main/java/amu/haimanot/censussystem4arbaminch/ManageData.java
@@ -7,16 +7,28 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import java.util.HashMap;
+
 public class ManageData extends AppCompatActivity {
 
+    private LinearLayout tabData;
+    private LinearLayout tabEnum;
+    private LinearLayout tabReport;
+    private Button btnDelete;
+    private Button btnEdit;
+    private GridLayout dataGrid;
+    private EditText etSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,32 +44,49 @@ public class ManageData extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        TableRow row=new TableRow(this);
-        row.setBackgroundColor(Color.GRAY);
-        TableRow.LayoutParams params=new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,TableRow.LayoutParams.MATCH_PARENT);
-        row.setLayoutParams(params);
-
-        TableLayout layout=(TableLayout)findViewById(R.id.tblFormData);
-        layout.setBackgroundColor(Color.GREEN);
-        TextView title1=new TextView(this);
-        title1.setText("Date");
-        TextView title2=new TextView(this);
-        title2.setText("Area");
-        TextView title3=new TextView(this);
-        title3.setText("No. Emurated");
-
-        GridLayout tempLayout=new GridLayout(this);
-        tempLayout.setColumnCount(3);
-        tempLayout.setRowCount(1);
-        tempLayout.addView(title1);
-        tempLayout.addView(title2);
-        tempLayout.addView(title3);
-
-        row.addView(tempLayout);
-        layout.addView(row);
-
-
-
+        etSearch=(EditText)findViewById(R.id.etSearchInput);
+        tabData=(LinearLayout)findViewById(R.id.Data);
+        tabEnum=(LinearLayout)findViewById(R.id.Enumerator);
+        tabReport=(LinearLayout)findViewById(R.id.Report);
+        btnDelete=(Button)findViewById(R.id.btnDelete);
+        btnEdit=(Button)findViewById(R.id.btnEdit);
+        etSearch.setOnEditorActionListener(
+                new TextView.OnEditorActionListener() {
+                    @Override
+                    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                        search(etSearch.getText().toString());
+                        return true;
+                    }
+                }
+        );
+    }
+    public void search(String term){
+        HashMap map=new HashMap();
+        //Pre process the input term
+        if(term.contains(" ")){
+            String[] temp=term.split(" ");
+            if(temp.length==2){
+                map.put("mode","2");
+                map.put("firstname",temp[0]);
+                map.put("lastname",temp[1]);
+            }
+            else if(temp.length==3){
+                map.put("mode","3");
+                map.put("firstname",temp[0]);
+                map.put("lastname",temp[1]);
+                map.put("middlename",temp[2]);
+            }
+        }
+        else if(term.startsWith("p")){
+            map.put("mode",4);
+            map.put("id",term);
+        }
+        else{
+            map.put("mode","1");
+            map.put("term",term);
+        }
+        //preprocessing complete
+    
     }
 
 }
